dd238283d6b4391d2f65e8badccd71b45267cbeb
emoncse2020 <emon.cse.2020@gmail.com>
2025-09-14 19:13:37 +0600

    note prop drilling done

---
+++ src/components/Note.jsx
@@ -0,0 +1,35 @@
+const Note = ({ note, deleteNote }) => {
+  return (
+    <div
+      key={note.id}
+      className={`p-4 bg-white rounded-lg shadow-md border-l-4 ${
+        note.priority === "High"
+          ? "border-l-red-500"
+          : note.priority === "Medium"
+          ? "border-l-yellow-500"
+          : note.priority === "Low"
+          ? "border-l-orange-500"
+          : "border-l-green-400"
+      }`}
+    >
+      <h2 className="text-lg font-bold">{note.title}</h2>
+      <p className="text-sm text-gray-500">
+        <strong>Category: </strong>
+        {note.category}
+      </p>
+      <p className="text-sm text-gray-500">
+        <strong>Priority: </strong>
+        {note.priority}
+      </p>
+      <p className="mt-2">{note.description}</p>
+      <button
+        className="mt-3 text-red-600 cursor-pointer hover:text-red-800 transition"
+        onClick={() => deleteNote(note.id)}
+      >
+        Delete👉🏻🗑️
+      </button>
+    </div>
+  );
+};
+
+export default Note;
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,3 +1,5 @@
+import Note from "./Note";
+
 const NoteList = ({ notes, deleteNote }) => {
   if (notes.length === 0) {
     <p className="text-center text-gray-500">No Notes Yet</p>;
@@ -5,35 +7,7 @@ const NoteList = ({ notes, deleteNote }) => {
   return (
     <div className="space-y-4">
       {notes.map((note) => (
-        <div
-          key={note.id}
-          className={`p-4 bg-white rounded-lg shadow-md border-l-4 ${
-            note.priority === "High"
-              ? "border-l-red-500"
-              : note.priority === "Medium"
-              ? "border-l-yellow-500"
-              : note.priority === "Low"
-              ? "border-l-orange-500"
-              : "border-l-green-400"
-          }`}
-        >
-          <h2 className="text-lg font-bold">{note.title}</h2>
-          <p className="text-sm text-gray-500">
-            <strong>Category: </strong>
-            {note.category}
-          </p>
-          <p className="text-sm text-gray-500">
-            <strong>Priority: </strong>
-            {note.priority}
-          </p>
-          <p className="mt-2">{note.description}</p>
-          <button
-            className="mt-3 text-red-600 cursor-pointer hover:text-red-800 transition"
-            onClick={() => deleteNote(note.id)}
-          >
-            Delete👉🏻🗑️
-          </button>
-        </div>
+        <Note key={note.id} note={note} deleteNote={deleteNote}></Note>
       ))}
     </div>
   );
